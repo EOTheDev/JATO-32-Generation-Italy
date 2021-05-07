086caf39ee4f199204d9d400760ab3241348bef7
EOTheDev <edo17maggio@gmail.com>
2021-05-07 11:55:05 +0200

    Create Pizza.java

---
+++ Java/Lez1/Pizza.java
@@ -0,0 +1,8 @@
+
+
+public class Pizza {
+    public static void main(String[] args) {
+        String margherita="la pizza margherita ha la passata, la mozzarella e due foglie di basilico";
+        System.out.println(margherita);
+    }
+}
