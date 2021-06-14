71c6033ccdfdb20e04cc7040cbee37c3835d2fe8
EOTheDev <edo17maggio@gmail.com>
2021-06-14 13:02:14 +0200

    lez DB

--- OscarVecchioneDataBase/condizioni.sql
+++ OscarVecchioneDataBase/condizioni.sql
@@ -8,3 +8,7 @@ from libro AS l, editore AS e, autore AS a, autore_libro AS al
 where e.id=l.editore_id
 and a.id=al.autore_id
 and l.id=al.libro_id;
+
+select cognome, date_format(data_nascita, "%d %m %Y") 'data nascita' from studente;
+
+select str_to_date(concat_ws("-","05","10","1969","%d-%m-%Y"));
