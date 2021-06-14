5bdbf9982b1f3b96da0fa0af7d25348d07294e96
EOTheDev <edo17maggio@gmail.com>
2021-06-14 11:41:41 +0200

    commit

---
+++ OscarVecchioneDataBase/condizioni.sql
@@ -0,0 +1,10 @@
+select titolo, prezzo, autore.nome, cognome, editore.nome
+from libro, editore, autore, autore_libro
+where editore.id=libro.editore_id
+and libro.id=autore_libro.libro_id;
+
+select titolo, prezzo, a.nome, cognome, e.nome AS Editore
+from libro AS l, editore AS e, autore AS a, autore_libro AS al
+where e.id=l.editore_id
+and a.id=al.autore_id
+and l.id=al.libro_id;
